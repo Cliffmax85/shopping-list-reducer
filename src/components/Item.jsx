67dbed9d0d119bb7d9eedfba6f24560453e87038
cliffmax85 <cliffmax85@gmail.com>
2022-05-13 16:32:01 -0700

    got strikethrough

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -8,24 +8,48 @@ export default function Item({ item, onUpdate, onDelete }) {
     if (isEditing) {
         content = (
             <form onSubmit={(event) => { event.preventDefault(); setIsEditing(false); }} >
-                <input aria-label='edit input' value={item.text} onChange={(e) => onUpdate({ ...item, text: e.target.value })}></input>
-                <button aria-label='save edits'>Save</button>  
+                <input 
+                aria-label='edit input' 
+                value={item.text}
+                onChange={(e) => onUpdate({ ...item, text: e.target.value })}></input>
+                <button aria-label='save edits'>
+                    Save
+                </button>  
             </form>
         )
     } else {
         content = (
-            <button aria-label={`edit button`} type='button' onClick={() => setIsEditing(true)} >Edit</button>
+            <>
+            <p style={{ textDecoration: item.done ? 'line-through' : null }}>
+                {item.text}
+            </p>
+            <button
+                aria-label={`edit button`}
+                type='button'
+                onClick={() => setIsEditing(true)}>
+                    Edit
+                </button>
+            </>
         )
     }
 
   return (
     <>
-        <input type='checkbox' checked={item.done} onChange={(e) => {
-            onUpdate({ ...item, done: e.target.checked });
+    <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+        <input 
+        type='checkbox' 
+        checked={item.done} 
+        onChange={(e) => {
+        onUpdate({ ...item, done: e.target.checked });
         }} />
-        {item.text}
         {content}
-        <button aria-label={`delete button`} type='button' onClick={() => onDelete(item.id)} >Delete Item</button>
+        <button 
+        aria-label={`delete button`} 
+        type='button' 
+        onClick={() => onDelete(item.id)} >
+            Delete Item
+        </button>
+    </div>
     </>
 )
 }
